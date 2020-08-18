8511a029a511df42f9dbfdc762ee998e0c67d837
MaxAkonde <devdevmaster@gmail.com>
2020-08-18 20:48:23 +0200

    edit post

---
+++ resources/views/profiles/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Modifier mes informations</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('profiles.update', ['user' => $user]) }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
+                        <div class="form-group">
+                            <label for="title">Titre</label>
+                            <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') ?? $user->profile->title }}" required autocomplete="title">
+                            @error('title')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" required autocomplete="description">{{ old('description') ?? $user->profile->description }}</textarea>
+                            @error('description')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="url">URL</label>
+                            <input id="url" type="text" class="form-control @error('url') is-invalid @enderror" name="url" value="{{ old('url') ?? $user->profile->url }}" required autocomplete="url">
+                            @error('url')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input @error('image') is-invalid @enderror" name="image" id="image">
+                                <label class="custom-file-label" for="image">Choisir une image ...</label>
+                                @error('image')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Modifier mes informations
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -16,6 +16,9 @@
                     <div class="mr-3"> <strong>951</strong> abonnés</div>
                     <div class="mr-3"> <strong>3</strong> abonnements</div>
                 </div>
+                @can('update', $user->profile)
+                    <a href="{{ route('profiles.edit', ['username' => $user->username]) }}" class="btn btn-outline-secondary mt-3">Modifier mes informations</a>
+                @endcan
                 <div class="mt-3">
                     <div class="font-weight-bold">{{ $user->profile->title }}</div>
                     <div>{{ $user->profile->description }}</div>
